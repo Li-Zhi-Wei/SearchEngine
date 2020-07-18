c5fd8930a11546ac120b77982a674994fc34ecfc
LiZhiWei <812568314@qq.com>
2020-07-18 22:50:04 +0800

    编写工具类中

---
+++ src/com/lizhiwei/common/io/PersistenceIndex.java
@@ -0,0 +1,25 @@
+package com.lizhiwei.common.io;
+
+import java.io.IOException;
+
+public interface PersistenceIndex {
+
+    /**
+     * 写入 返回值为文件的offset
+     *
+     * @param line
+     * @return
+     * @throws IOException
+     */
+    public long write(byte[] bytes);
+
+    /**
+     * 根据起止位置返回
+     *
+     * @param s
+     * @param e
+     * @return
+     */
+    public byte[] read(long s, long e);
+
+}
